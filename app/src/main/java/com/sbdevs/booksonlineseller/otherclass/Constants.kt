d55a0906388cd61464da3d860b0f1114eb9c78d4
bapi <nikhilesh16061999@gmail.com>
2022-03-28 20:31:57 +0530

    update_3.8

--- app/src/main/java/com/sbdevs/booksonlineseller/otherclass/Constants.kt
+++ app/src/main/java/com/sbdevs/booksonlineseller/otherclass/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
         const val SERVER_KEY = "key=AAAA1GsTats:APA91bH7rijTXYmtlUcPSRfrZVtF8qavAMjJa2kk1YXegaxV82ZbGItii4eBAr2dc1BDEsm80ceT5KIQtD2zGgkDO-nBgsBPV9EozRRnrvOmxLCob3txUWisB1hjMNKZphpFsXSVe6sM"
-        const val CONTENT_TYPE = "application/json"
+        const val CONTENT_TYPE_FCM = "application/json"
     }
 
 }
